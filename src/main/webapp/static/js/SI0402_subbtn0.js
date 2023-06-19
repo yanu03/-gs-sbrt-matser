472b606ae4af9e972e0fa67d01b20d2b1cf0e8bd
hwyang <hwyang@tracom.kr>
2023-06-19 18:35:27 +0900

    [양현우] 노선경로관리 상,하 버튼 수

--- src/main/webapp/static/js/SI0402_subbtn0.js
+++ src/main/webapp/static/js/SI0402_subbtn0.js
@@ -23,7 +23,8 @@
 	$('#subbtn0').bind('click', function(){
 		//상
 		let v_index = $.jf_curdgindex($('#dg1'));
-		if (v_index > 0) {
+		//그리드 전체 변경
+		/*if (v_index > 0) {
 			let v_data = $('#dg1').datagrid('getData');
 			let v_temp = v_data.rows[v_index - 1];
 			v_data.rows[v_index - 1] = v_data.rows[v_index];
@@ -34,15 +35,33 @@
 				v_vals = $.jf_singledatatojson('NODE_SN', i+1);
 				$('#dg1').datagrid('updateRow',{index:i,row:v_vals});
 			}
+		};*/
+		//현재 row와 위 row만 변경
+		if (v_index > 0) {
+			let v_data = $('#dg1').datagrid('getData');
+			let v_aboveRow = JSON.parse(JSON.stringify(v_data.rows[v_index - 1])); //위 row
+			let v_belowRow = JSON.parse(JSON.stringify(v_data.rows[v_index])); //선택된 row(아래 row)
+			
+			let v_aboveRowNodeSN = v_aboveRow['NODE_SN'];
+			let v_belowRowNodeSN = v_belowRow['NODE_SN'];
+			
+			v_aboveRow['NODE_SN'] = v_belowRowNodeSN;
+			v_belowRow['NODE_SN'] = v_aboveRowNodeSN;
+			
+			$('#dg1').datagrid('updateRow',{index:v_index,row:v_aboveRow});			
+			$('#dg1').datagrid('updateRow',{index:v_index-1,row:v_belowRow});			
+			$('#dg1').datagrid('selectRow', v_index - 1);
 		};
 	})
 	
 	$('#subbtn1').bind('click', function(){
 		//하
 		// $.jf_downdgdata($('#dg1'), 'NODE_SN');
+		
 		var v_index = $.jf_curdgindex($('#dg1'));
 		var v_data = $('#dg1').datagrid('getData');
-		if (v_index < v_data.rows.length - 1) {
+		//그리드 전체 변경
+		/*if (v_index < v_data.rows.length - 1) {
 			var v_temp = v_data.rows[v_index + 1];
 			v_data.rows[v_index + 1] = v_data.rows[v_index];
 			v_data.rows[v_index] = v_temp;
@@ -52,6 +71,24 @@
 				v_vals = $.jf_singledatatojson('NODE_SN', i+1);
 				$('#dg1').datagrid('updateRow',{index:i,row:v_vals});
 			}
+		}*/
+		
+		//현재 row와 아래 row만 변경
+		if (v_index < v_data.rows.length - 1) {
+			let v_data = $('#dg1').datagrid('getData');
+			
+			let v_aboveRow = JSON.parse(JSON.stringify(v_data.rows[v_index])); //위 row
+			let v_belowRow = JSON.parse(JSON.stringify(v_data.rows[v_index+1])); //선택된 row(아래 row)			
+			
+			let v_aboveRowNodeSN = v_aboveRow['NODE_SN'];
+			let v_belowRowNodeSN = v_belowRow['NODE_SN'];
+			
+			v_aboveRow['NODE_SN'] = v_belowRowNodeSN;
+			v_belowRow['NODE_SN'] = v_aboveRowNodeSN;
+
+			$('#dg1').datagrid('updateRow',{index:v_index,row:v_belowRow});			
+			$('#dg1').datagrid('updateRow',{index:v_index+1,row:v_aboveRow});			
+			$('#dg1').datagrid('selectRow', v_index + 1);
 		}
 	});
 	$('#subbtn2').bind('click', function(){
