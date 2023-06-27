1e967c85e179f7dc1243b25850da4a0b15949fdc
wonyong park <wypark@tracom.kr>
2023-06-27 00:15:47 +0000

    Update excel download

--- src/main/java/kr/tracom/controller/SI/SI0501Controller.java
+++ src/main/java/kr/tracom/controller/SI/SI0501Controller.java
@@ -62,6 +62,22 @@ public class SI0501Controller extends ControllerSupport {
 		return result.getResult();
 	}
 
-	
+	@RequestMapping("/si/SI0501G0_exlDownload")
+    public String  SI0501G0_exlDownload(Model model) throws Exception {
+
+		String[] getValues = {"STTN_ID", "STTN_NM", "AREA", "AREA_NM", "STTN_ENM", "STTN_NO", "GPS_Y", "GPS_X"
+				, "STTN_FCLT_TYPE", "STTN_FCLT_TYPE_NM", "VHC_DOOR_DIR_TYPE", "VHC_DOOR_DIR_TYPE_NM", "WAY_DIV", "WAY_DIV_NM"
+				, "BAY_TYPE", "BAY_TYPE_NM", "BAY_LEN", "LINE_CNT", "CENTER_YN", "STOP_TM_PEAK", "STOP_TM_NONE_PEAK", "USE_YN", "REMARK"};
+		String[] headerTitle = {"정류소ID", "정류소명", "권역", "권역 명", "정류소영문명", "정류소번호", "위도", "경도"
+				, "정류소시설유형", "정류소시설유형 명", "버스문방향", "버스문방향 명", "상하행", "상하행 명"
+				, "베이유형", "베이유형 명", "베이길이(m)", "차선수", "중앙차로여부", "첨두시 정차시간(초)", "비첨두시 정차시간(초)", "사용여부", "비고"};
+		
+		model.addAttribute("title", "정류소정보");
+		model.addAttribute("headerTitle", headerTitle);
+		model.addAttribute("getValues", getValues);
+		model.addAttribute("excelList", si0501Service.SI0501G0_exlDownload());
+		return "ExcelView";
+        //return new ModelAndView("ExcelView", "map", result);
+	}
 
 }
